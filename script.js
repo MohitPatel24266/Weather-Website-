cd5ab805c86f1562b5c50f423070156b94913436
MohitPatel24266 <mohitpatel24266@gmail.com>
2024-06-29 15:08:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+if (typeof window !== "undefined") {
+    
+    const apikey = "faa06b4145a864675760ee10312ee2a9";
+
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+    const searchBox = document.getElementById("#searchbar");
+
+    const searchBtn = document.querySelector("#btn-city");
+    // console.log(searchBtn.value);
+    async function checkWeather(city) {
+        const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+        var data = await response.json();
+        document.querySelector("#city") = data.name;
+        document.querySelector("#temp") = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity") = data.main.humidity + "%";
+        document.querySelector(".wind") = data.wind.speed + "km/h";
+        document.querySelector(".description") = data.weather[0].description;
+    };
+    // var res = searchBox.value;
+    searchBtn.addEventListener("click", () => {
+        checkWeather(searchBox.value);
+    });
+
+}
